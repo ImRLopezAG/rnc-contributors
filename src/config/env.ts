218f083b0e57b96013c48c856adfdf04a2d2956e
Angel Gabriel Lopez Solano <alopez@dextra.com.do>
2024-12-22 18:19:12 -0400

    fix: add step to list contents of zip file in deployment workflow and change default PORT to 8080

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,6 @@ loadEnvFile();
 const { ...processEnv } = process.env;
 
 export const env = () => (z.object({
-  PORT: z.string().transform((val) => parseInt(val, 10)).default('3000'),
+  PORT: z.string().transform((val) => parseInt(val, 10)).default('8080'),
   DATABASE_URL: z.string().default('file:./src/db/data.db'),
 }).parse(processEnv));
