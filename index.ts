3f2b4792c4bdb044ac1a528fd2cc4840c07d6347
ImRLopez <angelg00lopez@gmail.com>
2024-12-22 21:11:40 -0400

    refactor: remove old deployment workflow and add Vercel configuration

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { rncService } from '@services/rnc-service';
-import { readFile } from 'node:fs/promises'
-
-const file = await readFile('data.csv', 'utf-8');
-
-const contributors = rncService().getContributors(file);
-
-console.log(contributors.get('101591315'));
-
